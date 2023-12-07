16f9200128f8246996070300041c35437815d23f
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-12-08 01:08:35 +0500

    redux setup done

--- components/CustomProvider.jsx
+++ components/CustomProvider.jsx
@@ -1,11 +1,15 @@
 "use client";
 
+import store from "@/redux/store";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
 export default function CustomProvider({ children }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ChakraProvider>
+        <Provider store={store}>{children}</Provider>
+      </ChakraProvider>
     </CacheProvider>
   );
 }
